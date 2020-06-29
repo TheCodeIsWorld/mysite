37959d7fa3f2e7490d28507da6c48016bf25a9df
TheCodeIsWorld <35627468+TheCodeIsWorld@users.noreply.github.com>
2020-06-29 20:03:46 +0800

    try commit

--- stock/urls.py
+++ stock/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from . import views
 
 app_name = 'stock'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/', include('stock.api.urls')),
-    path('detail/<str:ts_code>', views.get_share, name='detail'),
-    path('rank_by_share_times', views.rank_by_share_times, name='rank_by_share_times'),
-    path('recent_shares', views.recent_shares, name='recent_shares'),
-    path('get_shares_by_time_point', views.get_shares_by_time_point, name='get_shares_by_time_point')
-]
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('api/', include('stock.api.urls')),
+#     path('detail/<str:ts_code>', views.get_share, name='detail'),
+#     path('rank_by_share_times', views.rank_by_share_times, name='rank_by_share_times'),
+#     path('recent_shares', views.recent_shares, name='recent_shares'),
+#     path('get_shares_by_time_point', views.get_shares_by_time_point, name='get_shares_by_time_point')
+# ]
